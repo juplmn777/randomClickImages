40b860d5387ba0d1e9a1b6e56df953ff35451bb2
ju <plumyoen.julien@gmail.com>
2020-02-14 16:09:56 +0100

    position de l'image définie & ok

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 addEventListener("click", function(event){
     const image = document.createElement("img");
-    const imageWidth = 100;
+    const imageWidth = 200;
     const imageHeight = 100;
 
     image.setAttribute("src", `http://lorempixel.com/${imageWidth}/${imageHeight}/`);
 
     image.style.position = "absolute";
-    
+    image.style.top = event.y - imageHeight / 2 + "px";
+    image.style.left = event.x - imageWidth / 2 + "px";
+
+    document.body.appendChild(image);
 })
