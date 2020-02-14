71a0f5e5750ea98442be7972113b314828093b53
ju <plumyoen.julien@gmail.com>
2020-02-14 16:02:37 +0100

    attribut src ajouté à l'élément img

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 addEventListener("click", function(event){
     const image = document.createElement("img");
+    const imageWidth = 100;
+    const imageHeight = 100;
+
+    image.setAttribute("src", `http://lorempixel.com/${imageWidth}/${imageHeight}/`)
+    
 })
